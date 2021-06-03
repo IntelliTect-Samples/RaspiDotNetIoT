ff5716658fadaeee2e1b4b071827d740272f38ae
Austen Frostad <austensaffron@gmail.com>
2021-06-02 19:41:04 -0700

    add annotaion

--- Pi.IO/ServoPin.cs
+++ Pi.IO/ServoPin.cs
@@ -11,11 +11,11 @@ namespace Pi.IO
         public const int _ServoClockDivisor = 384; // 19.2MHz/384 = 50Hz
         public const int _ServoPWMRange = 2000; // If pwmClock is 192 (19.2MHz) and pwmRange is 2000 we'll get the PWM frequency = 50
 
-        const int _ServoAngleMin = 0;  // Min pulse length out of 2000
-        const int _ServoAngleMax = 180;  // Max pulse length out of 2000
+        const int _ServoAngleMin = 0;  // Angle corresponding to _ServoRegisterMinPulse
+        const int _ServoAngleMax = 180;  // Angle corresponding to _ServoRegisterMaxPulse
 
-        const int _ServoRegisterMin = 25;  // Min pulse length out of 2000
-        const int _ServoRegisterMax = 110;  // Max pulse length out of 2000
+        const int _ServoRegisterMinPulse = 25;  // Min pulse length out of 2000
+        const int _ServoRegisterMaxPulse = 110;  // Max pulse length out of 2000
 
         public ServoPin(BcmPin bcmPin)
         {
@@ -33,7 +33,7 @@ namespace Pi.IO
 
             if (angle > _ServoAngleMax || angle < _ServoAngleMin) return;
 
-            _Pin.PwmRegister = (int)((double)angle / (double)_ServoAngleMax * (double)(_ServoRegisterMax-_ServoRegisterMin)) + _ServoRegisterMin;
+            _Pin.PwmRegister = (int)((double)angle / (double)_ServoAngleMax * (double)(_ServoRegisterMaxPulse-_ServoRegisterMinPulse)) + _ServoRegisterMinPulse;
 
             Console.WriteLine(_Pin.PwmRegister);
         }
